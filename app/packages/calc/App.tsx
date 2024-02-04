5eafeea853a3a990fade0d3c6cf2ae908deb560a
Timmy Jose <zoltan.jose@gmail.com>
2024-02-04 16:30:34 +0530

    Local and Push Notifications support.

--- app/packages/calc/App.tsx
+++ app/packages/calc/App.tsx
@@ -1,8 +1,105 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
-import { View, TextInput, Button, Text, StyleSheet, StatusBar } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { View, TextInput, Button, Text, StyleSheet, StatusBar, Platform } from 'react-native'
 import { execute } from 'react-native-rust-bridge'
 import * as Updates from 'expo-updates'
+import * as Notifications from 'expo-notifications'
+import * as Device from 'expo-device'
+import Constants from 'expo-constants'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync()
+      finalStatus = status
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!')
+      return
+    }
+
+    console.log(`projectId = ${Constants.expoConfig?.extra?.eas.projectId}`)
+    token = (await Notifications.getExpoPushTokenAsync({ projectId: Constants.expoConfig?.extra?.eas.projectId})).data
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
+}
+
+async function requestPermissionsAsync() {
+  return await Notifications.requestPermissionsAsync({ ios: { 
+    allowAlert: true,
+    allowBadge: true,
+    allowSound: true,
+    allowAnnouncements: true 
+    } 
+  }) 
+}
+
+async function allowsNotificationsAsync() {
+  let settings;
+
+  if (Platform.OS == 'ios') {
+    settings = await requestPermissionsAsync();
+
+  } else {
+    settings = await Notifications.getPermissionsAsync();
+  }
+  console.log('settings = ', settings)
+  return (
+    settings.granted || settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+  )
+}
+
+async function schedulePushNotification() {
+  const hasPushNotificationPermissionGranted = await allowsNotificationsAsync()
+  console.log(`hasPushNotificationPermissionGranted = ${hasPushNotificationPermissionGranted}`)
+
+  if (hasPushNotificationPermissionGranted) {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 2 },
+    });
+  } else {
+    console.log('`hasPushNotificationPermissionGranted is false`')
+  }
+}
+
+Notifications.scheduleNotificationAsync({
+  content: {
+    title: 'Look at that notification',
+    body: "I'm so proud of myself!",
+  },
+  trigger: null,
+});
+
 
 const App = () => {
  const [x, setX] = useState(0)
@@ -71,15 +168,43 @@ const App = () => {
   ? 'This app is running from built-in code'
   : 'This app is running another update'
 
+  const [expoPushToken, setExpoPushToken] = useState('')
+  const [notification, setNotification] = useState(false)
+  const notificationListener = useRef()
+  const responseListener = useRef()
+
+  useEffect(() => {
+      registerForPushNotificationsAsync().then(token => setExpoPushToken(token))
+  
+      notificationListener.current = Notifications.addNotificationReceivedListener((notification: boolean | ((prevState: boolean) => boolean)) => {
+        setNotification(notification)
+      });
+  
+      responseListener.current = Notifications.addNotificationResponseReceivedListener((response: any) => {
+        console.log(response)
+      });
+  
+      return () => {
+        Notifications.removeNotificationSubscription(notificationListener.current)
+        Notifications.removeNotificationSubscription(responseListener.current)
+      };
+    }, [])
 
  return (
    <View style={styles.container}>
     <Text>{runTypeMessage}</Text>
-    <Text>Testing after changing back to `production` channel and branch</Text>
-    <Text>Adding another line before eas update</Text>
-    <Text>Adding another line after eas update for Android testing</Text>
-    <Text>Testing Native code changes</Text>
-    <Text>This is new text added after the new build (post Native code changes)</Text>
+    <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification()
+        }}
+      />
      <TextInput
        testID='app-textinput-x'
        style={styles.input}
