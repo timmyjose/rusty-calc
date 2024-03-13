f088c854a3557a7978134af5e35d7eff4a7ac0e0
Timmy Jose <zoltan.jose@gmail.com>
2024-03-13 12:17:28 +0530

    Auto-detect ANDROID_NDK_HOME.

--- app/packages/react-native-rust-bridge/cpp/rustbridge/build.sh
+++ app/packages/react-native-rust-bridge/cpp/rustbridge/build.sh
@@ -38,27 +38,32 @@ fi
 
 # Android
 if [[ "$EAS_BUILD_PLATFORM" == "android" || -z "$EAS_BUILD_PLATFORM" ]]; then
-  echo "ANDROID_NDK_HOME = ${ANDROID_NDK_HOME}"
-  if [[ -z "$ANDROID_NDK_HOME" ]]
-  then
-    echo "ANDROID_NDK_HOME not detected. Attempting to create it..."
-    NDK_VERSION="26.1.10909125"
-    export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/$NDK_VERSION"
+  ANDROID_HOME=
+  ANDROID_NDK_HOME=
 
-    if [ -d "$ANDROID_NDK_HOME" ]; then
-        echo "ANDROID_NDK_HOME directory exists: $ANDROID_NDK_HOME"
-    else
-        echo "ANDROID_NDK_HOME directory does not exist: $ANDROID_NDK_HOME"
-    fi
-  else
-    echo "Detected existing ANDROID_NDK_HOME in the env: ${ANDROID_NDK_HOME}"
-    if [ -d "$ANDROID_NDK_HOME" ]; then
-        echo "ANDROID_NDK_HOME directory exists: $ANDROID_NDK_HOME"
-    else
-        echo "ANDROID_NDK_HOME directory does not exist: $ANDROID_NDK_HOME"
-    fi
+  if [[ -z "${ANDROID_HOME}" ]]; then
+    export ANDROID_HOME=~/Library/Android/sdk
   fi
-    
+
+  echo "ANDROID_HOME = ${ANDROID_HOME}"
+
+  if [[ ! -d "${ANDROID_HOME}"/ndk ]]; then
+    echo "No NDK installation found"
+    exit 1
+  fi
+
+  LATEST_NDK_VERSION=$(ls -f "${ANDROID_HOME}/ndk" | sort -V | tail -n 1)
+  echo "Latest NDK version = ${LATEST_NDK_VERSION}"
+
+  ANDROID_NDK_HOME="${ANDROID_HOME}/ndk/${LATEST_NDK_VERSION}"
+  echo "ANDROID_NDK_HOME= ${ANDROID_NDK_HOME}"
+
+  if [[ ! -d "${ANDROID_NDK_HOME}" ]]
+  then
+    echo "ANDROID_NDK_HOME directory does not exist. Aborting..."
+    exit 1
+  fi
+
   # NOTE for CI: this requires the `cargo-ndk`tool to be already installed.
   cargo ndk --target aarch64-linux-android --platform 21 -- build --release
   cargo ndk --target armv7-linux-androideabi --platform 21 -- build --release
