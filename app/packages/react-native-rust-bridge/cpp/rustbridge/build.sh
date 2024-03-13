023f60546342d6e41fbbc1da940829ddd8ad6f2b
Timmy Jose <zoltan.jose@gmail.com>
2024-03-13 15:06:51 +0530

    Refining workflows.

--- app/packages/react-native-rust-bridge/cpp/rustbridge/build.sh
+++ app/packages/react-native-rust-bridge/cpp/rustbridge/build.sh
@@ -19,6 +19,9 @@ fi
 rm -rf rustbridge.xcframework
 rm -rf ../target
 
+# optimise for all builds
+export RUSTFLAGS='-C embed-bitcode=yes -C lto=on' 
+
 # iOS
 if [[ "$EAS_BUILD_PLATFORM" == "ios" || -z "$EAS_BUILD_PLATFORM" ]]; then
     mkdir -p dist/aarch64-apple-ios
@@ -38,37 +41,63 @@ fi
 
 # Android
 if [[ "$EAS_BUILD_PLATFORM" == "android" || -z "$EAS_BUILD_PLATFORM" ]]; then
-  ANDROID_HOME=
-  ANDROID_NDK_HOME=
+  # We don't check for ANDROID_NDK_HOME directly since we want the latest version, 
+  # not the version that may have been set in the ANDROID_NDK_HOME env var.
+  ANDROID_HOME=${ANDROID_HOME:-""}
+
+  echo "Check ANDROID_HOME = ${ANDROID_HOME}"
 
   if [[ -z "${ANDROID_HOME}" ]]; then
-    export ANDROID_HOME=~/Library/Android/sdk
+    echo "ANDROID_HOME not set. Aborting..."
+    exit 1
   fi
 
   echo "ANDROID_HOME = ${ANDROID_HOME}"
 
-  if [[ ! -d "${ANDROID_HOME}"/ndk ]]; then
-    echo "No NDK installation found"
+  if [[ ! -d "${ANDROID_HOME}" ]]; then
+    echo "ANDROID_HOME directory does not exist. Aborting..."
     exit 1
   fi
 
-  LATEST_NDK_VERSION=$(ls -f "${ANDROID_HOME}/ndk" | sort -V | tail -n 1)
+  LATEST_NDK_VERSION=$(ls -f "${ANDROID_HOME}"/ndk | sort -V | tail -n 1)
   echo "Latest NDK version = ${LATEST_NDK_VERSION}"
 
   ANDROID_NDK_HOME="${ANDROID_HOME}/ndk/${LATEST_NDK_VERSION}"
-  echo "ANDROID_NDK_HOME= ${ANDROID_NDK_HOME}"
+  echo "Set ANDROID_NDK_HOME= ${ANDROID_NDK_HOME}"
 
-  if [[ ! -d "${ANDROID_NDK_HOME}" ]]
-  then
-    echo "ANDROID_NDK_HOME directory does not exist. Aborting..."
+  if [[ ! -d "${ANDROID_NDK_HOME}" ]]; then
+    echo "No NDK installation found. Aborting..."
     exit 1
   fi
 
   # NOTE for CI: this requires the `cargo-ndk`tool to be already installed.
-  cargo ndk --target aarch64-linux-android --platform 21 -- build --release
-  cargo ndk --target armv7-linux-androideabi --platform 21 -- build --release
-  cargo ndk --target i686-linux-android --platform 21 -- build --release
-  cargo ndk --target x86_64-linux-android --platform 21 -- build --release
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    SYSROOT_PATH="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android"
+    BINDGEN_EXTRA_CLANG_ARGS="--sysroot ${SYSROOT_PATH}" cargo ndk --bindgen --target aarch64-linux-android --platform 21 -- build --release
+  else
+    cargo ndk --bindgen --target aarch64-linux-android --platform 21 -- build --release
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    SYSROOT_PATH="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/armv7-linux--androideabi"
+    BINDGEN_EXTRA_CLANG_ARGS="--sysroot ${SYSROOT_PATH}" cargo ndk --bindgen --target armv7-linux-androideabi --platform 21 -- build --release
+  else
+    cargo ndk --bindgen --target armv7-linux-androideabi --platform 21 -- build --release
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    SYSROOT_PATH="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/i686-linux-android"
+    BINDGEN_EXTRA_CLANG_ARGS="--sysroot ${SYSROOT_PATH}" cargo ndk --bindgen --target i686-linux-android --platform 21 -- build --release
+  else
+    cargo ndk --bindgen --target i686-linux-android --platform 21 -- build --release
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    SYSROOT_PATH="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/x86_64-linux-android"
+    BINDGEN_EXTRA_CLANG_ARGS="--sysroot ${SYSROOT_PATH}" cargo ndk --bindgen --target x86_64-linux-android --platform 21 -- build --release
+  else
+    cargo ndk --bindgen --target x86_64-linux-android --platform 21 -- build --release
+  fi
 
   mkdir -p ../target/aarch64-linux-android/release
   cp target/aarch64-linux-android/release/librustbridge.a ../target/aarch64-linux-android/release/
