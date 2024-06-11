901ee3428f5f0d84fccd26a747f2bed762a8734d
Timmy Jose <zoltan.jose@gmail.com>
2024-06-11 22:05:47 +0530

    feat: added a Text field to test updates

--- app/packages/calc/Main.tsx
+++ app/packages/calc/Main.tsx
@@ -71,6 +71,7 @@ const Main = () => {
 
   return (
     <View style={styles.container}>
+      <Text>Testing updates</Text>
       <Text>Hello from Github!</Text>
       <Button testID='app-button-backup' title='Backup' onPress={() => navigation.navigate('Backup')}/>
       <Button testID='app-button-version-number' title='VersionNumber' onPress={() => navigation.navigate('VersionNumberInfo')}/>
